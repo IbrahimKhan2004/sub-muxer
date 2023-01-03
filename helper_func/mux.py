d27fd274b3ead868e809e479f08ae61e9e385eb8
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2023-01-03 21:50:30 +0530

    Update mux.py

--- helper_func/mux.py
+++ helper_func/mux.py
@@ -108,7 +108,7 @@ async def hardmux_vid(vid_filename, sub_filename, msg):
             'ffmpeg','-hide_banner',
             '-i',vid,
             '-vf','subtitles='+sub,
-            '-c:v','h264',
+            '-c:v','h265',
             '-map','0:v:0',
             '-map','0:a:0?',
             '-preset','veryfast',
